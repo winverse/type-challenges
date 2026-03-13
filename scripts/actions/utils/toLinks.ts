a862569b3a8852a6c8a3317580cec7240df4b5ef
winverse <public.winverse@gmail.com>
2026-03-13 14:01:18 +0900

    chore: Remove various scripts related to issue/PR automation, localization, and project generation.

--- scripts/actions/utils/toLinks.ts
+++
@@ -1,9 +0,0 @@
-import type { Quiz } from '../../types'
-import type { SupportedLocale } from '../../locales'
-import { t } from '../../locales'
-import { toReadmeShort } from '../../toUrl'
-
-export function toLinks(quiz: Quiz, locale: SupportedLocale) {
-  return '\n\n'
-    + `> ${t(locale, 'link.view-on-github')}${toReadmeShort(quiz.no, locale)}`
-}
--- scripts/utils/resolve.ts
+++
@@ -1,8 +0,0 @@
-import { defaultLocale } from '../locales'
-
-export function resolveFilePath(dir: string, name: string, ext: string, locale: string) {
-  if (locale === defaultLocale)
-    return `${dir}/${name}.${ext}`
-  else
-    return `${dir}/${name}.${locale}.${ext}`
-}
