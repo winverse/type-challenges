607a6f8a1bb6c57a90aa42a2142c0645aec0e37b
winverse <public.winverse@gmail.com>
2026-03-29 14:20:46 +0900

    fix(questions): update type definitions for readonly, tuple, first, length, exclude, and awaited templates style: add Prettier configuration file

--- questions/02-easy-00011-tuple-to-object/template.ts
+++ questions/02-easy-00011-tuple-to-object/template.ts
@@ -1,1 +1,1 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly PropertyKey[]> = { [P in T[number]]: P }
--- questions/02-easy-00189-awaited/template.ts
+++ questions/02-easy-00189-awaited/template.ts
@@ -1,1 +1,1 @@
-type MyAwaited<T> = any
+type MyAwaited<T> = T extends { then: (onfulfilled: (arg: infer U) => any) => any } ? MyAwaited<U> : T
